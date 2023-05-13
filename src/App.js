974cc518b1a7e2da4d93caafabdfe6f3d07c29a0
MatMarrr <mateuszrmarek@gmail.com>
2023-05-13 16:19:15 +0200

    Add board and square component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import { useState } from "react";
+import Square from "./Components/Square";
 
 function App() {
-  return <div className="App"></div>;
+  const [board, setBoard] = useState(["", "", "", "", "", "", "", "", ""]);
+
+  return (
+    <div className="App">
+      <div className="board">
+        <div className="row"></div>
+        <div className="row"></div>
+        <div className="row"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
